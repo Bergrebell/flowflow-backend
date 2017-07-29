9abffe5ef75cc04445d8797a58a9490acd494181
Flo <florian.unternaehrer@simplificator.com>
2017-07-29 13:35:09 +0200

    add stub to test, so we don't spam the API

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'webmock'
+end
+
 group :production do
   gem 'rails_12factor'
 end
--- test/jobs/import_xml_job_test.rb
+++ test/jobs/import_xml_job_test.rb
@@ -1,17 +1,23 @@
 require 'test_helper'
+require 'webmock/minitest'
 
 class ImportXmlJobTest < ActiveJob::TestCase
+
   setup do
     @job = ImportXmlJob.new
-    @sample_path = 'test/support/hydrodata_excerpt.xml'
   end
 
   test 'perform' do
-    @job.perform(@sample_path)
+    stub_request(:get, "https://www.hydrodata.ch/data/hydroweb.xml")
+      .to_return(:status => 200, :body => open('test/support/hydrodata_excerpt.xml'))
+
+    @job.perform
+
     assert_equal 240, Station.count
     assert_equal 508, Measurement.count
 
     assert_equal 'Porte du Scex', Station.find_by(number: '2009').name
     assert_equal 9.0, Station.find_by(number: '2009').temperatures.first.value
   end
+
 end
