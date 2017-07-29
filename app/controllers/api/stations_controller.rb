e6617464d937b2f7b4348049dd3273fd326dec93
Flo <florian.unternaehrer@simplificator.com>
2017-07-29 13:36:28 +0200

    replace water-type with name in the measurements API

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -1,6 +1,6 @@
 class Api::StationsController < ApplicationController
   def index
-    stations = Station.all.as_json(only: [:id, :name, :water_body_type])
+    stations = Station.all.as_json(only: [:id, :name, :water_body_name])
     render json: stations
   end
 end
