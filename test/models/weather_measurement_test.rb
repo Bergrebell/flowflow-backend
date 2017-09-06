82718f1006c86d0daab3aa92407940d93e21b064
Bergrebell <rrkuepper@gmail.com>
2017-09-06 14:49:15 +0200

    adds test for weather_measurement model

--- test/models/weather_measurement_test.rb
+++ test/models/weather_measurement_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class WeatherMeasurementTest < ActiveSupport::TestCase
+  def setup
+    @stg = weather_measurements(:stg)
+  end
 
   test 'valid indicators' do
-    skip
+    expected_json = {
+      :air_temp=>16.0,
+      :wind_speed=>15.8,
+      :indicator=>"sun_cloud" }
+
+    assert_equal @stg.as_json, expected_json
   end
 
 end
