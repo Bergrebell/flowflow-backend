06732ac8d5b9e10869a28d7c6ad7111668c5c3c2
Bergrebell <rrkuepper@gmail.com>
2017-09-18 14:13:00 +0200

    adds middleware to compress api responses

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module HydroDaten
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Deflater
   end
 end
