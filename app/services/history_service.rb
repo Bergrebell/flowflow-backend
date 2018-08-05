f6e8f17c773e3adb509c444cf722bc729f5fe66c
Serge Hänni <serge.haenni@simplificator.com>
2018-08-05 15:32:58 +0200

    Added basic Ruby implementation of history service.

--- app/services/history_service.rb
+++ app/services/history_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # {
 #   waterlevel: [{
 #     value: 125, // aktueller wert
@@ -7,7 +9,6 @@
 #   temperatur: []
 # }
 class HistoryService
-
   def initialize(station)
     @station = station
   end
@@ -16,10 +17,28 @@ class HistoryService
     serialize
   end
 
+  def history
+    %w[Temperature Discharge SeaLevel Level DischargeLiter].map do |type|
+      {
+        type.downcase.pluralize => @station
+          .measurements
+          .where(datetime: (59.days.ago..Date.tomorrow))
+          .where(type: type)
+          .order(:datetime)
+          .pluck(:value)
+          .to_a
+          .each_cons(3)
+          .with_index
+          .select { |_measurements, index| (index % 3).zero? }
+          .map { |measurements, _index| measurements.sum.fdiv(3) }
+      }
+    end.reduce({}) { |hash, measurements| hash.merge(measurements) }
+  end
+
   private
 
   def serialize
-    measurement_types = %w(temperatures discharges sea_levels levels discharge_liters)
+    measurement_types = %w[temperatures discharges sea_levels levels discharge_liters]
     measurements = {}
     measurement_types.each do |m|
       measurements[m.camelize.downcase] = @station.send(m).less_than_week_old.map do |d|
@@ -30,11 +49,11 @@ class HistoryService
         }
       end
     end
+
     measurements
   end
 
   def weekly_average(m)
     @station.send(m).less_than_week_old.average(:value).to_f
   end
-
 end
--- test/services/history_service_test.rb
+++ test/services/history_service_test.rb
@@ -1,20 +1,34 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class HistoryServiceTest < ActiveJob::TestCase
   def setup
-    @basel = stations(:basel)
+    weather_station = WeatherStation.create!(number: 0, village: 'Test village', easting: 0, northing: 0)
+    @station = Station.create!(name: 'History Test Station', weather_station: weather_station, water_body_name: 'Test water', water_body_type: 'test', number: 0)
+
+    (59.days.ago.to_date..Date.tomorrow).each_with_index do |day, index|
+      Measurement.create!(
+        type: 'Temperature',
+        datetime: day + 1.days,
+        value: index,
+        unit: 'celsius',
+        station: @station
+      )
+    end
   end
 
   test 'should return correct serialized history data' do
     expected_serialized_history = {
-        "temperatures"=>[{:value=>21.0, :weeklyAverage=>22.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time},
-                         {:value=>23.0, :weeklyAverage=>22.0, :datetime=>'Wed, 08 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "discharges"=>[{:value=>3.0, :weeklyAverage=>3.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "sealevels"=>[{:value=>246.0, :weeklyAverage=>246.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "levels"=>[{:value=>100.0, :weeklyAverage=>100.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "dischargeliters"=>[{:value=>12.0, :weeklyAverage=>12.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}]
+      'temperatures' => [
+        1.0, 4.0, 7.0, 10.0, 13.0, 16.0, 19.0, 22.0, 25.0, 28.0, 31.0, 34.0, 37.0, 40.0, 43.0, 46.0, 49.0, 52.0, 55.0, 58.0
+      ],
+      'discharges' => [],
+      'sealevels' => [],
+      'levels' => [],
+      'dischargeliters' => []
     }
-    assert_equal expected_serialized_history, HistoryService.new(@basel).call
-  end
 
+    assert_equal expected_serialized_history, HistoryService.new(@station).history
+  end
 end
