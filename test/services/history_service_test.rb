c700cd59bcfa1e3a64d6ee0640ad3e920699cb7e
Serge Hänni <serge.haenni@simplificator.com>
2018-10-18 11:39:04 +0200

    Fixed history test.
    We don't return the whole history anymore, but only the most recent
    ones.

--- test/services/history_service_test.rb
+++ test/services/history_service_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class HistoryServiceTest < ActiveJob::TestCase
@@ -6,15 +8,17 @@ class HistoryServiceTest < ActiveJob::TestCase
   end
 
   test 'should return correct serialized history data' do
-    expected_serialized_history = {
-        "temperatures"=>[{:value=>21.0, :weeklyAverage=>22.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time},
-                         {:value=>23.0, :weeklyAverage=>22.0, :datetime=>'Wed, 08 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "discharges"=>[{:value=>3.0, :weeklyAverage=>3.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "sealevels"=>[{:value=>246.0, :weeklyAverage=>246.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "levels"=>[{:value=>100.0, :weeklyAverage=>100.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}],
-        "dischargeliters"=>[{:value=>12.0, :weeklyAverage=>12.0, :datetime=>'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time}]
-    }
-    assert_equal expected_serialized_history, HistoryService.new(@basel).call
-  end
+    travel_to reference_date do
+      expected_serialized_history = {
+        'temperatures' => [{ value: 21.0, weeklyAverage: 22.0, datetime: 'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time },
+                           { value: 23.0, weeklyAverage: 22.0, datetime: 'Wed, 08 Nov 2017 23:00:00 UTC +00:00'.to_time }],
+        'discharges' => [{ value: 3.0, weeklyAverage: 3.0, datetime: 'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time }],
+        'sealevels' => [{ value: 246.0, weeklyAverage: 246.0, datetime: 'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time }],
+        'levels' => [{ value: 100.0, weeklyAverage: 100.0, datetime: 'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time }],
+        'dischargeliters' => [{ value: 12.0, weeklyAverage: 12.0, datetime: 'Thu, 09 Nov 2017 23:00:00 UTC +00:00'.to_time }]
+      }
 
+      assert_equal expected_serialized_history, HistoryService.new(@basel).call
+    end
+  end
 end
