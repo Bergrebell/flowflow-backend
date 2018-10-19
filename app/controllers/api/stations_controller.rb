533a883790eee614dc8e5b7b6c941119d057ff90
Serge Hänni <serge.haenni@simplificator.com>
2018-10-19 10:21:40 +0200

    Changed according to `master` branch.

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
-class Api::StationsController < ApplicationController
-  before_action :set_station, only: %i[weather history]
+module Api
+  class StationsController < ApplicationController
+    before_action :set_station, only: %i[weather history]
 
-  def index
-    # Client.joins(:orders).where('orders.created_at' => time_range)
-    stations = Station.all
-                      .joins(:measurements)
-                      .where('measurements.datetime >= ?', 1.day.ago)
-                      .distinct
-                      .sort_by { |station| station.name }
-                      .map(&:serialize)
+    def index
+      # Client.joins(:orders).where('orders.created_at' => time_range)
+      stations = Station.all
+                        .joins(:measurements)
+                        .where('measurements.datetime >= ?', 1.day.ago)
+                        .distinct
+                        .sort_by { |station| station.name }
+                        .map(&:serialize)
 
-    render json: stations
-  end
+      render json: stations
+    end
 
-  def show
-    @station = Station.find(params[:id])
+    def show
+      @station = Station.find(params[:id])
 
-    render json: StationSerializerWithMeasurements.new(@station).as_json
-  end
+      render json: StationSerializerWithMeasurements.new(@station).as_json
+    end
 
-  def weather
-    weather_measurement = @station.weather_station.weather_measurement.serialize
-    render json: weather_measurement
-  end
+    def weather
+      weather_measurement = @station.weather_station.weather_measurement.serialize
+      render json: weather_measurement
+    end
 
-  def history
-    history = HistoryService.new(@station).call
-    render json: history
-  end
+    def history
+      history = HistoryService.new(@station).call
+      render json: history
+    end
 
-  private
+    private
 
-  def set_station
-    @station = Station.find(station_params[:id])
-  end
+    def set_station
+      @station = Station.find(station_params[:id])
+    end
 
-  def station_params
-    params.permit(
-      :id
-    )
+    def station_params
+      params.permit(
+        :id
+      )
+    end
   end
 end
